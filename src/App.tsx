c44c606c972ca4fc3b3d2f90d8029f6ce5728f12
小熊熊 <2085605952@qq.com>
2022-11-24 11:14:53 +0800

    更换背景颜色

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.scss';
 import { GithubLink } from './components/GithubLink/GithubLink';
+import { Heading } from './components/Heading/Heading';
 import PasswordGenerator from './components/PasswordGenerator/PasswordGenerator';
 
 function App() {
@@ -10,6 +11,10 @@ function App() {
   return (
     <div className="App">
       <div className="password-generator-wrapper">
+        <Heading
+          title='React.ts 密码生成器'
+          description='使用React.ts创建一个密码生成器的简单示例。' 
+        />
         <PasswordGenerator />
         <GithubLink />
       </div>
